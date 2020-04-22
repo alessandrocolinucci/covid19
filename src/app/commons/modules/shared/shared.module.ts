cf8ab23f2aca9b2b8d6e4d23303f8b1cc5ca473b
Alessandro Colinucci <a.colinucci@gmail.com>
2020-04-22 20:24:31 +0200

    DateAgo pipe added.

--- src/app/commons/modules/shared/shared.module.ts
+++ src/app/commons/modules/shared/shared.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MortalityRatePopoverComponent } from './components/mortality-rate-popover/mortality-rate-popover.component';
+import { DateAgoPipe } from './pipes/date-ago.pipe';
 
 @NgModule({
   declarations: [
-    MortalityRatePopoverComponent
+    MortalityRatePopoverComponent,
+    DateAgoPipe
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    MortalityRatePopoverComponent
+    MortalityRatePopoverComponent,
+    DateAgoPipe
   ],
   entryComponents: [
     MortalityRatePopoverComponent
