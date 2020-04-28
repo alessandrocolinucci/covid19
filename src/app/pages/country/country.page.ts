3b89415abe27df552cc271943f9ef85f372ad921
SimpleNetworks <simplenetworks@Mac-mini-di-SimpleNetworks-2.local>
2020-04-28 10:01:55 +0200

    Chart js library added

--- src/app/pages/country/country.page.ts
+++ src/app/pages/country/country.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CountryStatus } from 'src/app/commons/models/country-status';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-country',
@@ -9,7 +10,11 @@ import { CountryStatus } from 'src/app/commons/models/country-status';
 })
 export class CountryPage implements OnInit {
 
+  @ViewChild("lineChart", { static: true }) lineChart: ElementRef;
   countryStatus: CountryStatus;
+
+  bars: any;
+  colorArray: any;
 
   constructor(
     private route: ActivatedRoute
@@ -18,7 +23,42 @@ export class CountryPage implements OnInit {
   ngOnInit() {
     console.log(this.route.snapshot.data["countryStatus"]);
     this.countryStatus = this.route.snapshot.data.countryStatus;
+    this.createLineChart();
+  }
 
+  createLineChart() {
+    if (this.lineChart) {
+      this.lineChart = new Chart(this.lineChart.nativeElement, {
+        type: "line",
+        data: {
+          labels: ["January", "February", "March", "April", "May", "June", "July"],
+          datasets: [
+            {
+              label: "My First dataset",
+              fill: false,
+              lineTension: 0.1,
+              backgroundColor: "rgba(75,192,192,0.4)",
+              borderColor: "rgba(75,192,192,1)",
+              borderCapStyle: "butt",
+              borderDash: [],
+              borderDashOffset: 0.0,
+              borderJoinStyle: "miter",
+              pointBorderColor: "rgba(75,192,192,1)",
+              pointBackgroundColor: "#fff",
+              pointBorderWidth: 1,
+              pointHoverRadius: 5,
+              pointHoverBackgroundColor: "rgba(75,192,192,1)",
+              pointHoverBorderColor: "rgba(220,220,220,1)",
+              pointHoverBorderWidth: 2,
+              pointRadius: 1,
+              pointHitRadius: 10,
+              data: [65, 59, 80, 81, 56, 55, 40],
+              spanGaps: false
+            }
+          ]
+        }
+      });
+    }
   }
 
 }
